51d921e96e5860251a8222dab3fd65af384a0a89
pheonix_23 <hedahdia.faycel@gmail.com>
2025-06-12 17:46:32 +0100

    update api url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_WWoB63wkH/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
